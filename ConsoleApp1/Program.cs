1606648612b8eac909efa4578e143767d98890e2
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-17 16:04:07 +0300

    First commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,36 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /*Console.WriteLine("Ekrandan girilen sayıya kadar tek sayıları ekrana yazdır");
+          int sayi = Convert.ToInt32(Console.ReadLine());
+          for (int i = 1; i <=sayi; i++)
+          {
+              if (i%2==1)
+              {
+                  Console.WriteLine(i);
+              }
+          }
+          Console.ReadLine();*/
+
+
+
+            int tektoplam = 0;
+            int cifttoplam = 0;
+            Console.WriteLine("1 ile 1000 arasındaki tek ve çift sayıların toplamlarını ekrana göster");
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    cifttoplam += i;
+                }
+                else if (i % 2 == 1)
+                {
+                    tektoplam += i;
+                }
+            }
+            Console.WriteLine("Tek sayıların toplamı: " + tektoplam);
+            Console.WriteLine("Çift sayıların toplamı: " + cifttoplam);
+            Console.ReadLine();
         }
     }
 }
